0ae26ebd29a53dbb628c1e07e63edf9f3e1062b3
hairizuanbinnoorazman <hairizuanbinnoorazman@gmail.com>
2014-11-10 00:44:08 +0800

    Yamba
    Create repo for Yamba project

--- Yamba/Yamba/src/com/Hairizuan/yamba/StatusActivity.java
+++ Yamba/Yamba/src/com/Hairizuan/yamba/StatusActivity.java
@@ -1,8 +1,8 @@
-package com.Hairizuan.first_project;
+package com.Hairizuan.yamba;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,18 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
-	
-	int	tempGitIntTrial;
-	String trial = "yahoo";
-	
+public class StatusActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_status);
 
 		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
+			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
@@ -31,7 +28,7 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.status, menu);
 		return true;
 	}
 
@@ -58,8 +55,8 @@ public class MainActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_status,
+					container, false);
 			return rootView;
 		}
 	}
